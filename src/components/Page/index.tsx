f0cf5366f231e61a1b760a639aeb70df5e19ee8c
Jarilton <jariltonpereira02@hotmail.com>
2021-12-29 17:35:04 -0300

    Projeto Homepage Tesla

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react';
+
+import { Container } from './styles';
+
+const Page: React.FC = () => {
+  return (
+    <Container>
+      <h1>Page</h1>
+    </Container>
+  );
+};
+
+export default Page;
